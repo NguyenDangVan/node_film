898b298bdc051e9eec51633b855a79a42c6b8117
Dang Van <nguyen.dang.van@sun-asterisk.com>
2019-06-03 12:13:07 +0700

    Model user

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express')
-const mongoose = require('mongoose')
 const dotenv = require('dotenv')
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const expressValidator = require('express-validator')
+const routes = require('./src/routes/routes')
+const mongoose = require('mongoose')
 
 const app = express()
 const PORT = process.env.PORT || 8797
@@ -9,10 +13,16 @@ const db = mongoose.connection
 dotenv.config()
 
 //connect db
-mongoose.connect(process.env.DB_URL, { userNewURLParser: true}).then(() => console.log('DB connected!!'))
+mongoose.connect(process.env.DB_URL, { useNewURLParser: true}).then(() => console.log('DB connected!!'))
 db.on('error', (err) => {
     console.log('DB connection error:', err.message)
 })
+
+app.use(morgan("dev"))
+app.use(bodyParser.json())
+app.use(expressValidator())
+
+app.use('/', routes)
 
 app.use('/', (req, res) => {
     res.json({"mess": "Hello Would Van dep  ahihi!"})
@@ -24,3 +34,15 @@ app.listen(PORT, () => {
 })
 
 module.exports = app;
+
+const session = require('express-session')
+const MongoStore = require('connect-mongo')(session)
+
+app.use(session({
+    secret: 'work hard',
+    resave: true,
+    saveUninitialized: false,
+    store: new MongoStore({
+        mongooseConnection: db
+    })
+}))
--- src/models/UserModels.js
+++ src/models/UserModels.js
@@ -2,5 +2,11 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const userSchema = new Schema ({
-    
-})
+    email: {type: String, unique: true, required: true, trim: true},
+    name: {type: String, required: true, trim: true, minlength: 2, maxlength: 30},
+    role: {type: String, enum: ['admin', 'customer']},
+    password: {type: String, required: true, trim: true, minlength: 6},
+    password_confirm: {type: String, required: true, trim: true, minlength: 6}
+});
+
+module.exports = mongoose.model('User', userSchema)
