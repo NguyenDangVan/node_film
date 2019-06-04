ea80f4c1d70cc82836a2d7d4f6c66854dec469f2
Dang Van <dinvvan@gmail.com>
2019-06-05 00:13:17 +0700

    setup views - design home page

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -2,42 +2,42 @@ const User = require('../models/UserModels')
 const bcrypt = require('bcrypt')
 
 exports.register = function(req, res, next) {
-    User.findOne({email: req.body.email}, (err, user) => {
-        if (user = null) { //Kiểm tra xem email đã được sử dụng chưa
-            bcrypt.hash(req.body.password, 10, function (err, hash) { //Mã hóa mật khẩu trước khi lưu vào db
-                if (err) {return next(err);}
-                const user = new User(req.body)
-                user.role = ['customer'] //sau khi register thì role auto là customer
-                user.password = hash
-                user.password_confirm = hash
-                user.save((err, result) => {
-                    if (err) {return res.json({err})}
-                    res.json({user: result})
-                })
-            })
-        } else {
-            res.json({err: 'Email has been used'})
-        }
-    })
+  User.findOne({email: req.body.email}, (err, user) => {
+    if (user = null) { //Kiểm tra xem email đã được sử dụng chưa
+      bcrypt.hash(req.body.password, 10, function (err, hash) { //Mã hóa mật khẩu trước khi lưu vào db
+        if (err) {return next(err);}
+        const user = new User(req.body)
+        user.role = ['customer'] //sau khi register thì role auto là customer
+        user.password = hash
+        user.password_confirm = hash
+        user.save((err, result) => {
+            if (err) {return res.json({err})}
+            res.json({user: result})
+        })
+      })
+    } else {
+      res.json({err: 'Email has been used'})
+    }
+  })
 }
 
 exports.login = function (req, res) {
-    User.findOne({email: req.body.email}).exec(function(err, user) {
-        if (err) {
-            return res.json({err})
-        } else if (!user) {
-            return res.json({err: 'Username and Password are incorrect'})
-        }
-        bcrypt.compare(req.body.password, user.password, (err, result) => {
-            if(result === true){
-                req.session.user = user
-                res.json({
-                    user: user,
-                    "login": "sucess"
-                })
-            } else {
-                return res.json({err: 'Username and Password are incorrect!'})
-            }
+  User.findOne({email: req.body.email}).exec(function(err, user) {
+    if (err) {
+      return res.json({err})
+    } else if (!user) {
+      return res.json({err: 'Username and Password are incorrect'})
+    }
+    bcrypt.compare(req.body.password, user.password, (err, result) => {
+      if(result === true){
+        req.session.user = user
+        res.json({
+            user: user,
+            "login": "sucess"
         })
+      } else {
+        return res.json({err: 'Username and Password are incorrect!'})
+      }
     })
+  })
 }
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -9,19 +9,28 @@ const router = express.Router()
 
 const { register, login, logout } = require('../controllers/UserController')
 
+function requiresLogin(req, res, next) {
+  if (req.session && req.session.user) {
+    return next()
+  } else {
+    return res.json({err: 'You must be logged in to view this page.'})
+  }
+}
+
 function requiresLogout(req, res, next) {
-    if (req.session && req.session.user) {
-        return res.json({ err: 'You must be Logout in to Login continue' })
-    } else {
-        return next()
-    }
+  if (req.session && req.session.user) {
+    return res.json({ err: 'You must be logout in to Login continue' })
+  } else {
+    return next()
+  }
 }
 
 const { listPost, detailPost, createPost, editPost, deletePost } = require('../controllers/PostControllers')
 const { PostValidator, UserValidator } = require('../validators/validators')
-/*
-...
-*/
+
+router.get('/', (req, res) => {
+  res.render('home')
+})
 router.get('/posts', requiresLogin, listPost)
 router.get('/post/:id', requiresLogin, detailPost)
 router.post('/post/new', requiresLogin, PostValidator, createPost)
